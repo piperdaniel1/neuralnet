642e2d556e8f66f8b1104d73bbb5e118d3d07d84
Daniel Piper <piperdaniel1@gmail.com>
2021-12-15 17:58:43 -0800

    neural net backtest

--- shape_test.py
+++ shape_test.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy.core.numeric import full
 import pandas as pd
 import pandas_datareader as web
 import datetime as dt
@@ -10,6 +11,21 @@ from tensorflow.keras.layers import Dense, LSTM, Dropout
 from tensorflow.keras.models import Sequential
 import tensorflow as tf
 
+def gen_model():
+    model = Sequential()
+    model.add(LSTM(units=50, return_sequences=True, input_shape=(60, 1)))
+    model.add(Dropout(0.2))
+    model.add(LSTM(units=50, return_sequences=True))
+    model.add(Dropout(0.2))
+    model.add(LSTM(units=50))
+    model.add(Dropout(0.2))
+    model.add(Dense(units=1))
+
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    latest = tf.train.latest_checkpoint(checkpoint_dir)
+    model.load_weights(latest)
+
+    return model
 
 # Train the model
 checkpoint_path = "training_2/cp-{epoch:04d}.ckpt"
@@ -18,68 +34,26 @@ checkpoint_dir = os.path.dirname(checkpoint_path)
 crypto_currency = 'BTC'
 against_currency = 'USD'
 
-start = dt.datetime(2016, 1, 1)
-end = dt.datetime.now()
+model = gen_model()
+offset = dt.timedelta(days=60)
+test_start = dt.datetime(2020, 1, 1) - offset
+test_end = test_start + dt.timedelta(days=90)
 
-data = web.DataReader(crypto_currency + "-" + against_currency, 'yahoo', start, end)
+data = web.DataReader(crypto_currency + "-" + against_currency, 'yahoo', test_start, test_end)
 scaler = MinMaxScaler(feature_range=(0, 1))
 scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1, 1))
 
-prediction_days = 60
+full_data = scaled_data[0:60]
+c = 0
+while c < 30:
+    test_data = full_data[-60:]
+    test_data = np.reshape(test_data, (1, 60, 1))
+    new_pred = model.predict(test_data)
+    full_data = np.append(full_data, new_pred)
+    c+=1
 
-x_train, y_train = [], []
+full_data = scaler.inverse_transform(full_data.reshape(-1, 1))
 
-for x in range(prediction_days, len(scaled_data)):
-    x_train.append(scaled_data[x-prediction_days:x, 0])
-    y_train.append(scaled_data[x, 0])
-
-x_train, y_train = np.array(x_train), np.array(y_train)
-x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-
-model = Sequential()
-model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
-model.add(Dropout(0.2))
-model.add(LSTM(units=50, return_sequences=True))
-model.add(Dropout(0.2))
-model.add(LSTM(units=50))
-model.add(Dropout(0.2))
-model.add(Dense(units=1))
-
-model.compile(optimizer='adam', loss='mean_squared_error')
-latest = tf.train.latest_checkpoint(checkpoint_dir)
-model.load_weights(latest)
-
-
-# Test the model
-test_start = dt.datetime(2020, 1, 1)
-test_end = dt.datetime.now()
-
-test_data = web.DataReader(crypto_currency + "-" + against_currency, 'yahoo', test_start, test_end)
-actual_prices = test_data['Close'].values
-
-total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)
-
-model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediction_days:].values
-model_inputs = model_inputs.reshape(-1, 1)
-model_inputs = scaler.fit_transform(model_inputs)
-
-x_test = []
-
-for x in range(prediction_days, len(model_inputs)):
-    x_test.append(model_inputs[x-prediction_days:x, 0])
-
-x_test = np.array(x_test)
-x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
-
-prediction_prices = model.predict(x_test)
-prediction_prices = scaler.inverse_transform(prediction_prices)
-
-
-# Plot the result
-plt.plot(actual_prices, color='black', label='Actual Prices')
-plt.plot(prediction_prices, color='green', label='Predicted Prices')
-plt.title('BTC-USD prediction')
-plt.xlabel('Time')
-plt.ylabel('Price')
-plt.legend(loc='upper left')
+plt.plot(full_data, color='blue')
+plt.plot(scaler.inverse_transform(scaled_data), color='red')
 plt.show()
---
+++ test3.py
@@ -0,0 +1,84 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy.core.numeric import full
+import pandas as pd
+import pandas_datareader as web
+import datetime as dt
+import os
+
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.layers import Dense, LSTM, Dropout
+from tensorflow.keras.models import Sequential
+import tensorflow as tf
+
+def gen_model():
+    model = Sequential()
+    model.add(LSTM(units=50, return_sequences=True, input_shape=(60, 1)))
+    model.add(Dropout(0.2))
+    model.add(LSTM(units=50, return_sequences=True))
+    model.add(Dropout(0.2))
+    model.add(LSTM(units=50))
+    model.add(Dropout(0.2))
+    model.add(Dense(units=1))
+
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    latest = tf.train.latest_checkpoint(checkpoint_dir)
+    model.load_weights(latest)
+
+    return model
+
+# Train the model
+checkpoint_path = "training_2/cp-{epoch:04d}.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+crypto_currency = 'BTC'
+against_currency = 'USD'
+
+model = gen_model()
+total_profit = 0
+money = 10000
+c=0
+while dt.datetime(2020, 1, 1) + dt.timedelta(days=7) * c + dt.timedelta(days=67) < dt.datetime.now():
+    offset = dt.timedelta(days=7) * c
+    test_start = dt.datetime(2020, 1, 1) + offset
+    test_end = test_start + dt.timedelta(days=67)
+
+    data = web.DataReader(crypto_currency + "-" + against_currency, 'yahoo', test_start, test_end)
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1, 1))
+
+    full_data = scaled_data[0:60]
+    next_point = model.predict(np.reshape(full_data, (1, 60, 1)))
+
+    pred_close = scaler.inverse_transform(next_point)
+    actual_close = scaler.inverse_transform(scaled_data[-1:])
+    current_close = scaler.inverse_transform(full_data[-1:])
+
+    print("Prediction for period: " + str(test_start) + " to " + str(test_end))
+    if current_close < pred_close:
+        print("Price will go up!")
+        profit_pct = (actual_close - current_close) / current_close
+        print("Profit percentage if followed predicition: " + str(round(profit_pct[0][0] * 100, 2)) + "%")
+        total_profit += round(profit_pct[0][0] * 100, 2)
+    elif current_close > pred_close:
+        print("Price will go down!")
+        profit_pct = ((actual_close - current_close) / current_close) * -1
+        print("Profit percentage if followed predicition: " + str(round(profit_pct[0][0] * 100, 2)) + "%")
+        total_profit += round(profit_pct[0][0] * 100, 2)
+    else:
+        print("Price will stay the same!")
+        print("Profit percentage if followed predicition: 0%")
+
+    money *= (1 + profit_pct[0][0])
+    print("Current close: $" + str(current_close))
+    print("Predicted one week close: $" + str(scaler.inverse_transform(next_point)))
+    print("Actual one week close: $" + str(scaler.inverse_transform(scaled_data[-1:])))
+    print("Difference %: " + str(((scaler.inverse_transform(next_point) - scaler.inverse_transform(scaled_data[-1:])) / scaler.inverse_transform(scaled_data[-1:])) * 100))
+    print("Total profit: $" + str(total_profit))
+    print("Money: $" + str(money))
+    print("\n")
+    #plt.plot(scaler.inverse_transform(scaled_data), color='blue')
+    #plt.plot(scaler.inverse_transform(full_data), color='green')
+    #plt.scatter(67, scaler.inverse_transform(next_point), color='red')
+    #plt.show()
+    c+=1
